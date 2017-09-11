40cf636f90f9495452a34e66330c90a68197a3e4
李正军 <leezj9671@gmail.com>
2017-09-11 08:33:50 -0500

    Create download.py

---
+++ download.py
@@ -0,0 +1,20 @@
+# encoding=utf-8
+import requests
+import re
+headers = {
+    # 自行修改
+    'Cookie': 'UM_distinctid=15e6f0a8d67fb-01c3287b338fea-3c310551-100200-15e6f0a8d6828; CNZZDATA1254124561=1559124444-1505101164-http%253A%252F%252Fujs.91job.gov.cn%252F%7C1505117383; PHPSESSID2=taqp5pjj99llgli1dour6t4nc1',
+}
+bt = 100
+url = 'http://ujs.91job.gov.cn/contest/question?page='
+with open('tiku.txt', 'w') as f:
+    f.write('github.com/leezj9671\n')
+    for i in range(1, 729):
+        r = requests.get(url + str(i), headers=headers).content
+        r = r.decode('utf-8')
+        title = re.findall('<b><font color="red">(.*?)</font><br/>&nbsp;&nbsp;&nbsp;(.*?)</b>', r, re.S)
+        choices = re.findall('<label>(.*?)</label>', r, re.S)
+        answer = re.findall('<p><font color="red"><b>(.*?)</b></font></p>', r)[0]
+        f.write('\n'.join(title[0]) + '\n')
+        f.write('\n'.join(choices) + '\n')
+        f.write(answer + '\n\n')
