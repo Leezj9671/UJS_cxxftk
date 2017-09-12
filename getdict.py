6a769b0b193dd82bfaa1b89221c3a87b379c28d7
lizhengjun <leezj9671@gmail.com>
2017-09-12 21:58:52 +0800

    getdict add

---
+++ getdict.py
@@ -0,0 +1,23 @@
+import re
+
+findans = False
+istitle = False
+ansdict = {}
+title = ''
+with open('tiku.txt', 'rb') as f:
+    for line in f.readlines():
+        line = line.decode('utf-8')
+        if istitle:
+            title = line.strip()
+            istitle = False
+            continue
+        if findans:
+            ans = re.findall('正确答案:(\w+)', line)
+            if ans != []:
+                ansdict[title] = ans[0]
+                findans = False
+        else:
+            t = re.match('第(\d+)题', line)
+            if t:
+                findans = True
+                istitle = True
