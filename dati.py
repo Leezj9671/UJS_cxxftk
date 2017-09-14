cbc7bd84880fafc67ad0a39f94559924a2d499ef
Leezj9671 <leezj9671@gmail.com>
2017-09-14 16:23:27 +0800

    make dati.py structors clear&headers add

--- dati.py
+++ dati.py
@@ -1,10 +1,15 @@
 # 建议在中文环境下运行，否则会出现codec错误
 import requests
 import re
+from time import sleep
+
+burl = 'http://nuist.91job.gov.cn'
+startusername = 3140604015
+endusername = 3140604016
+pausetime = 1
 
 def run(username):
     password = username[-6:] if username else ''
-
     headers = {
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
     }
@@ -17,7 +22,6 @@ def run(username):
         'loginsubmit': '1',
     }
 
-    burl = 'http://ujs.91job.gov.cn'
     url = burl + '/contest/answer'
     lurl = burl + '/user/login?referer=/contest/answer&callback=login'
     surl = burl + '/contest/savedata'
@@ -26,7 +30,7 @@ def run(username):
     s.post(lurl, data=data, headers=headers)
 
     # 检测是否已经答完题
-    html = s.get(url).text
+    html = s.get(url, headers=headers).text
     score = re.findall('<b><font color="red">(.*?)</font></b>', html)
     if score != []:
         print(username, score[0], score[1])
@@ -35,9 +39,9 @@ def run(username):
     # 确认已经答完题后，导入题库开始搞事
     from getdict import ansdict
     cnt = 0
-    nxtu = s.get(url).url
+    nxtu = s.get(url, headers=headers).url
     while True:
-        html = s.get(nxtu).text
+        html = s.get(nxtu, headers=headers).text
 
         try:
             tid = re.findall('tid=(\d+)', nxtu)[0]
@@ -61,22 +65,23 @@ def run(username):
         s.post(surl, data=data, headers=headers).text
 
         nxtu = re.findall('<a (.*?) href="(.*?)">下一题</a>', html, re.S)
-        if nxtu == []:
+        if not nxtu:
             break
         else:
             nxtu = burl + nxtu[0][1].replace('\n', '')
+            sleep(pausetime)
 
     #交卷啦
     jjurl = re.findall('<a(.*?) href="(.*?)">交(.*?)卷</a>', html, re.S)
-    if jjurl != []:
+    if not jjurl:
         jjurl = burl + jjurl[0][1]
-        html = s.get(jjurl)
+        html = s.get(jjurl, headers=headers)
     print(username, '答题完毕')
-    html = s.get(url).text
+    html = s.get(url, headers=headers).text
     score = re.findall('<b><font color="red">(.*?)</font></b>', html)
-    if score != []:
+    if not score:
         print(username, score[0], score[1])
 
 if __name__ == '__main__':
-    for username in range(123, 123):
+    for username in range(startusername, endusername):
         run(str(username))
